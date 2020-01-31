ca06aafc03f8c72773ca3f60f83a49d3e2ecf70e
Dmitriy Khodakivsky <d.khodakivsky@whirl.sg>
2020-01-31 17:41:01 +0200

    fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World pitys!');
+  res.send('Hello Worldd pitys!');
 });
 
 var server = app.listen(3000, function () {
